42d78aa9949f0fac96a3c43414b2882b9f0b84e6
Saurabh Pandey <119298223+Saurabh8227@users.noreply.github.com>
2024-07-15 02:24:37 +0530

    Add files via upload

---
+++ arraypassing.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[10];
+    cout<<"Values of comparison : "<<endl;
+    cout<<"The values of a is "<<a<<endl;
+    cout<<"The value of address a[0] is "<<&a[0]<<endl;
+    cout<<"The value of address a[1] is "<<&a[1]<<endl;
+}
